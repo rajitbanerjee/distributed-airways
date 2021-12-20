943cee634c2ca17811a966a1b4893ab741ea980e
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-12-20 11:40:20 +0000

    Initial working version

---
+++ src/test/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
