c4bf6fc03f9d16717a637d73526e225c5fd20516
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2021-12-21 21:13:16 +0000

    Implement basic graphql server for emirates

---
+++ emirates/src/main/java/com/distributed/airways/entity/Flight.java
@@ -0,0 +1,23 @@
+package com.distributed.airways.entity;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+    private String id;
+    private String airline;
+    private List<String> flightNumber;
+    private List<String> time;
+    private String sourceCity;
+    private String destinationCity;
+    private String sourceAirport;
+    private String destinationAirport;
+    private List<String> dayOfWeek;
+    private List<Double> price;
+    // private List<String> category;
+}
---
+++ emirates/src/main/java/com/distributed/airways/graphql/GraphQLProvider.java
@@ -0,0 +1,55 @@
+package com.distributed.airways.graphql;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import java.io.IOException;
+import java.net.URL;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphQLProvider {
+
+    @Autowired GraphQLDataFetchers graphQLDataFetchers;
+
+    private GraphQL graphQL;
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private GraphQLSchema buildSchema(String sdl) throws IOException {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() throws IOException {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(
+                        newTypeWiring("Query")
+                                .dataFetcher(
+                                        "flights", graphQLDataFetchers.getFlightsDataFetcher()))
+                .build();
+    }
+
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+}
