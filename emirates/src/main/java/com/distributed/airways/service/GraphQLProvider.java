3ebe068fd060900846ea36c6d0a989acf175e346
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2021-12-22 02:54:22 +0000

    Implement spring boot mongodb connection for emirates

--- emirates/src/main/java/com/distributed/airways/service/GraphQLProvider.java
+++ emirates/src/main/java/com/distributed/airways/service/GraphQLProvider.java
@@ -1,10 +1,15 @@
-package com.distributed.airways.graphql;
+package com.distributed.airways.service;
 
 import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
+import com.distributed.airways.model.EmiratesFlight;
+import com.distributed.airways.repository.FlightRepository;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import com.jsoniter.JsonIterator;
+import com.jsoniter.spi.TypeLiteral;
 import graphql.GraphQL;
+import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
@@ -12,6 +17,7 @@ import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -20,16 +26,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class GraphQLProvider {
 
-    @Autowired GraphQLDataFetchers graphQLDataFetchers;
+    @Autowired EmiratesService service;
+    @Autowired FlightRepository flightRepository;
 
     private GraphQL graphQL;
+    private List<EmiratesFlight> flights;
+    private static final String SCHEMA = "schema.graphqls";
+    private static final String JSON = "flights.json";
 
     @PostConstruct
     public void init() throws IOException {
-        URL url = Resources.getResource("schema.graphqls");
+        URL url = Resources.getResource(SCHEMA);
         String sdl = Resources.toString(url, Charsets.UTF_8);
         GraphQLSchema graphQLSchema = buildSchema(sdl);
         this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        populateRepository();
+    }
+
+    private void populateRepository() {
+        try {
+            URL url = Resources.getResource(JSON);
+            String jsonString = Resources.toString(url, Charsets.UTF_8);
+            flights =
+                    JsonIterator.deserialize(
+                            jsonString, new TypeLiteral<List<EmiratesFlight>>() {});
+            flightRepository.deleteAll();
+            flightRepository.saveAll(flights);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private GraphQLSchema buildSchema(String sdl) throws IOException {
@@ -40,11 +65,9 @@ public class GraphQLProvider {
     }
 
     private RuntimeWiring buildWiring() throws IOException {
+        DataFetcher<List<EmiratesFlight>> fetcher = service.getFlightsDataFetcher(flightRepository);
         return RuntimeWiring.newRuntimeWiring()
-                .type(
-                        newTypeWiring("Query")
-                                .dataFetcher(
-                                        "flights", graphQLDataFetchers.getFlightsDataFetcher()))
+                .type(newTypeWiring("Query").dataFetcher("flights", fetcher))
                 .build();
     }
 
