6f16f1855446049acbdfc33ec7530778bde0b940
noorb98 <noorbari1998@hotmail.com>
2021-12-23 02:58:57 +0000

    Update src/main/java/com/distributed/airways/service/CathayPacific.java

--- src/main/java/com/distributed/airways/service/CathayPacific.java
+++ src/main/java/com/distributed/airways/service/CathayPacific.java
@@ -11,10 +11,10 @@ public class CathayPacific {
     public void applyTicketClassPremiums(List<Flight> flights) {
         for (Flight f : flights) {
             if (f.getCategory().equals("Business")) {
-                System.out.println("HELLOOOO2O");
+                
                 f.setPrice(f.getPrice() + 50);
             } else if (f.getCategory().equals("Premium")) {
-                System.out.println("HELLOOOO3O");
+                
                 f.setPrice(f.getPrice() + 100);
             }
         }
