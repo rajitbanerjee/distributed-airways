305c6566ccc36fdc072e8e47eacae4ae5da05c9f
Rajit Banerjee <rajit.banerjee@ucdconnect.ie>
2021-12-24 00:02:10 +0000

    Minor refactor/cleanup

--- emirates/src/main/java/com/distributed/airways/Application.java
+++ emirates/src/main/java/com/distributed/airways/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Starting airline service: Emirates");
         SpringApplication.run(Application.class, args);
     }
 }
--- emirates/src/main/java/com/distributed/airways/service/EmiratesService.java
+++ emirates/src/main/java/com/distributed/airways/service/EmiratesService.java
@@ -3,18 +3,18 @@ package com.distributed.airways.service;
 import com.distributed.airways.model.EmiratesFlight;
 import com.distributed.airways.repository.FlightRepository;
 import com.distributed.airways.utils.DateFormatter;
+import com.google.common.collect.ImmutableMap;
 import graphql.schema.DataFetcher;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmiratesService {
     private List<EmiratesFlight> flights;
-    private static final String BUSINESS_CLASS = "Business";
-    private static final String FIRST_CLASS = "First";
-    private static final int BUSINESS_PREMIUM = 5;
-    private static final int FIRST_PREMIUM = 8;
+    private static final Map<String, Integer> TICKET_CLASS_PREMIUMS =
+            ImmutableMap.of("Economy", 1, "Business", 5, "First", 8);
 
     public DataFetcher<List<EmiratesFlight>> getFlightsDataFetcher(
             FlightRepository flightRepository) throws IOException {
@@ -40,14 +40,10 @@ public class EmiratesService {
     private void applyTicketClassPremiums() {
         for (EmiratesFlight flight : flights) {
             List<Double> prices = flight.getPrice();
-            Double economyPrice = prices.get(0);
+            Double economyPrice = prices.remove(0);
 
             for (String ticketClass : flight.getCategory()) {
-                if (ticketClass.equals(BUSINESS_CLASS)) {
-                    prices.add(economyPrice * BUSINESS_PREMIUM);
-                } else if (ticketClass.equals(FIRST_CLASS)) {
-                    prices.add(economyPrice * FIRST_PREMIUM);
-                }
+                prices.add(economyPrice * TICKET_CLASS_PREMIUMS.getOrDefault(ticketClass, 1));
             }
 
             flight.setPrice(prices);
